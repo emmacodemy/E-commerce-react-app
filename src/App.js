4e734c0969f5c4b46256cdd90a26395c3d1f1a95
emmacodemy <eayodeleof18@gmail.com>
2020-07-23 17:38:31 +0100

    create-react-app crown ecommerce and setup homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {HomePage} from "../src/pages/Homepage" ;
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+export const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
