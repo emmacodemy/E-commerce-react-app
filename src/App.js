025347310656fcd82bdd195ae160d9adf0619b63
emmacodemy <eayodeleof18@gmail.com>
2020-07-23 22:48:51 +0100

    broken homepage into components and finish menu item components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {HomePage} from "../src/pages/Homepage" ;
+import {HomePage} from "./pages/homepage/Homepage";
 
 function App() {
   return (
