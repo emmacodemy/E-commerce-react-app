876c65516f74a3ca3688896199fea689db5fb457
emmacodemy <eayodeleof18@gmail.com>
2020-08-19 21:10:32 +0100

    updating stripe integration and updating our favicon and title

--- src/App.js
+++ src/App.js
@@ -73,3 +73,6 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// 4242 4242 4242 4242
+// 01/20 CW 123
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -9,6 +9,7 @@ import {
 
 import "./checkout.styles.scss";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -33,8 +34,14 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>TOTAL: ${total}</span>{" "}
+      <span>TOTAL: ${total}</span>
     </div>
+    <div className="text-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    {cartItems.length ? <StripeCheckoutButton price={total} /> : null}
   </div>
 );
 
